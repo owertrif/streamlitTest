fbfbed3816e293c25faee9c8b7ce02e4959058af
owertrif <nightfilin43@gmail.com>
2024-05-19 18:29:29 +0300

    first attempt

--- streamlitTest.py
+++ streamlitTest.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+st.write('Here is my first attempt to visualise table: ')
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
