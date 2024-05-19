cd5a9ed55273a5ca44d90ba700d90b81928cff25
owertrif <nightfilin43@gmail.com>
2024-05-19 18:42:01 +0300

    first attempt

--- streamlitTest.py
+++ streamlitTest.py
@@ -2,5 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['A', 'B', 'C'])
-st.table(chart_data)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
