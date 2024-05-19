8e5d0493a4dd96c519e859eae64345607ab68d60
owertrif <nightfilin43@gmail.com>
2024-05-19 18:51:58 +0300

    first attempt

--- streamlitTest.py
+++ streamlitTest.py
@@ -2,6 +2,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-if st.checkbox('Show dataframe'):
-  chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-  chart_data
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [5, 6, 7, 8]
+})
+
+option = st.selectbox(
+  'Which column do you want?',
+  df['first column']
+)
+
+'You chose: ',option
