ba3ba23054a0e14bc5d1cc4b42b8f8664b2d5ac0
owertrif <nightfilin43@gmail.com>
2024-05-19 18:32:19 +0300

    first attempt

--- streamlitTest.py
+++ streamlitTest.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import pandas as pd
-st.write('Here is my first attempt to visualise table: ')
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+import numpy as np
 
-df
+dataframe = np.random.randn(10,20)
+st.dataframe(dataframe)
