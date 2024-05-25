1cfb2dffbfcdff5707aa5b5c3077d27b8cc6d645
owertrif <nightfilin43@gmail.com>
2024-05-25 13:51:03 +0300

    first attempt

--- streamlitTest.py
+++ streamlitTest.py
@@ -19,16 +19,16 @@ data = load_data(10000)
 
 data_load_state.text("Done! (using st.cache_data)")
 
-st.subheader('Raw data')
-st.write(data)
+if st.checkbox('Show data'):
+  st.subheader('Raw data')
+  st.write(data)
 
-st.subheader('Nubrer of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour,bins=24,range=(0,24))[0]
-st.bar_chart(hist_values)
+if st.checkbox('Show bar chart'):
+  st.subheader('Nubrer of pickups by hour')
+  hist_values = np.histogram(data[DATE_COLUMN].dt.hour,bins=24,range=(0,24))[0]
+  st.bar_chart(hist_values)
 
 hours_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hours_to_filter]
 st.subheader(f'Map of all pickups at {hours_to_filter}:00')
 st.map(filtered_data)
-st.pydeck_chart(filtered_data)
-
